9a8cad95951e0bf5c2ebb8a027eee66dd462285d
Jesse van Bree <jessevanbree@live.nl>
2019-11-28 23:30:19 +0100

    implemented calculateMostInvolvedEmployees

--- src/main/java/PPS.java
+++ src/main/java/PPS.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -20,177 +21,17 @@ public class PPS {
     private Set<Employee> employees;
     private Set<Project> projects;
 
-    @Override
-    public String toString() {
-        return String.format("PPS_e%d_p%d", this.employees.size(), this.projects.size());
-    }
-
     private PPS() {
         this.name = "none";
         this.planningYear = 2000;
         this.projects = new TreeSet<>();
         this.employees = new TreeSet<>();
     }
+
     private PPS(String resourceName, int year) {
         this();
         this.name = resourceName;
         this.planningYear = year;
-    }
-
-    /**
-     * Reports the statistics of the project planning year
-     */
-    public void printPlanningStatistics() {
-        System.out.printf("\nProject Statistics of '%s' in the year %d\n",
-                this.name, this.planningYear);
-        if (this.employees == null || this.projects == null ||
-                this.employees.size() == 0 || this.projects.size() == 0) {
-            System.out.println("No employees or projects have been set up...");
-            return;
-        }
-
-        System.out.printf("%d employees have been assigned to %d projects:\n\n",
-                this.employees.size(), this.projects.size());
-
-        // TODO calculate and display statistics
-
-    }
-
-    /**
-     * calculates the average hourly wage of all known employees in this system
-     * @return
-     */
-    public double calculateAverageHourlyWage() {
-        // TODO
-        return 0.0;
-    }
-
-    /**
-     * finds the project with the highest number of available working days.
-     * (if more than one project with the highest number is found, any one is returned)
-     * @return
-     */
-    public Project calculateLongestProject() {
-        // TODO
-        return null;
-    }
-
-    /**
-     * calculates the total budget for assigned employees across all projects and employees in the system
-     * based on the registration of committed hours per day per employee,
-     * the number of working days in each project
-     * and the hourly rate of each employee
-     * @return
-     */
-    public int calculateTotalManpowerBudget() {
-        // TODO
-        return 0;
-    }
-
-    /**
-     * finds the employees that are assigned to the highest number of different projects
-     * (if multiple employees are assigned to the same highest number of projects,
-     * all these employees are returned in the set)
-     * @return
-     */
-    public Set<Employee> calculateMostInvolvedEmployees() {
-        // TODO
-        return null;
-    }
-
-    /**
-     * Calculates an overview of total managed budget per employee that complies with the filter predicate
-     * The total managed budget of an employee is the sum of all man power budgets of all projects
-     * that are being managed by this employee
-     * @param filter
-     * @return
-     */
-    public Map<Employee,Integer> calculateManagedBudgetOverview(Predicate<Employee> filter) {
-        // TODO
-        return null;
-    }
-
-    /**
-     * Calculates and overview of total monthly spends across all projects in the system
-     * The monthly spend of a single project is the accumulated manpower cost of all employees assigned to the
-     * project across all working days in the month.
-     * @return
-     */
-    public Map<Month,Integer> calculateCumulativeMonthlySpends() {
-        // TODO
-        return null;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * A builder helper class to compose a small PPS using method-chaining of builder methods
-     */
-    public static class Builder {
-        PPS pps;
-
-        public Builder() {
-            this.pps = new PPS();
-        }
-
-        /**
-         * Add another employee to the PPS being build
-         * @param employee
-         * @return
-         */
-        public Builder addEmployee(Employee employee) {
-            // TODO maybe works
-            pps.employees.add(employee);
-            return this;
-        }
-
-        /**
-         * Add another project to the PPS
-         * register the specified manager as the manager of the new
-         * @param project
-         * @param manager
-         * @return
-         */
-        public Builder addProject(Project project, Employee manager) {
-            // TODO maybe works
-            manager.getAssignedProjects().add(project);
-            pps.projects.add(project);
-            return this;
-        }
-
-        /**
-         * Add a commitment to work hoursPerDay on the project that is identified by projectCode
-         * for the employee who is identified by employeeNr
-         * This commitment is added to any other commitment that the same employee already
-         * has got registered on the same project,
-         * @param projectCode
-         * @param employeeNr
-         * @param hoursPerDay
-         * @return
-         */
-        public Builder addCommitment(String projectCode, int employeeNr, int hoursPerDay) {
-            // TODO
-            return this;
-        }
-
-        /**
-         * Complete the PPS being build
-         *
-         * @return
-         */
-        public PPS build() {
-            return this.pps;
-        }
-    }
-
-    public Set<Project> getProjects() {
-        return this.projects;
-    }
-
-    public Set<Employee> getEmployees() {
-        return this.employees;
     }
 
     /**
@@ -220,5 +61,185 @@ public class PPS {
         }
 
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("PPS_e%d_p%d", this.employees.size(), this.projects.size());
+    }
+
+    /**
+     * Reports the statistics of the project planning year
+     */
+    public void printPlanningStatistics() {
+
+        System.out.printf("\nProject Statistics of '%s' in the year %d\n",
+                this.name, this.planningYear);
+        if (this.employees == null || this.projects == null ||
+                this.employees.size() == 0 || this.projects.size() == 0) {
+            System.out.println("No employees or projects have been set up...");
+            return;
+        }
+
+        System.out.printf("%d employees have been assigned to %d projects:\n\n",
+                this.employees.size(), this.projects.size());
+
+        // TODO calculate and display statistics
+        System.out.println(calculateMostInvolvedEmployees().toString());
+
+    }
+
+    /**
+     * calculates the average hourly wage of all known employees in this system
+     *
+     * @return
+     */
+    public double calculateAverageHourlyWage() {
+        // TODO
+        return 0.0;
+    }
+
+    /**
+     * finds the project with the highest number of available working days.
+     * (if more than one project with the highest number is found, any one is returned)
+     *
+     * @return
+     */
+    public Project calculateLongestProject() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * calculates the total budget for assigned employees across all projects and employees in the system
+     * based on the registration of committed hours per day per employee,
+     * the number of working days in each project
+     * and the hourly rate of each employee
+     *
+     * @return
+     */
+    public int calculateTotalManpowerBudget() {
+        // TODO
+        return 0;
+    }
+
+    /**
+     * finds the employees that are assigned to the highest number of different projects
+     * (if multiple employees are assigned to the same highest number of projects,
+     * all these employees are returned in the set)
+     *
+     * @return
+     */
+    public Set<Employee> calculateMostInvolvedEmployees() {
+        // 2 Versions possible
+            // 1 With hardcoded limit on minimum assigned projects
+//          return employees.stream().filter(
+//          (employee) -> employee.getAssignedProjects().size()>= 10 )
+//          .collect( Collectors.toCollection( () -> new TreeSet<>( Comparator.comparing(Employee::getName) ) ) );
+
+        // 2 based on the employee with the highest number of assigned projects
+        return employees.stream().filter(
+                (employee) -> employee.getAssignedProjects().size()
+                        >= employees.stream().mapToInt( (e) -> e.getAssignedProjects().size() ).max().getAsInt() )
+                .collect( Collectors.toCollection( () -> new TreeSet<>( Comparator.comparing(Employee::getName) ) ) );
+    }
+
+    /**
+     * Calculates an overview of total managed budget per employee that complies with the filter predicate
+     * The total managed budget of an employee is the sum of all man power budgets of all projects
+     * that are being managed by this employee
+     *
+     * @param filter
+     * @return
+     */
+    public Map<Employee, Integer> calculateManagedBudgetOverview(Predicate<Employee> filter) {
+        // TODO J
+        return null;
+    }
+
+    /**
+     * Calculates and overview of total monthly spends across all projects in the system
+     * The monthly spend of a single project is the accumulated manpower cost of all employees assigned to the
+     * project across all working days in the month.
+     *
+     * @return
+     */
+    public Map<Month, Integer> calculateCumulativeMonthlySpends() {
+        // TODO J
+        return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Project> getProjects() {
+        return this.projects;
+    }
+
+    public Set<Employee> getEmployees() {
+        return this.employees;
+    }
+
+    /**
+     * A builder helper class to compose a small PPS using method-chaining of builder methods
+     */
+    public static class Builder {
+        PPS pps;
+
+        public Builder() {
+            this.pps = new PPS();
+        }
+
+        /**
+         * Add another employee to the PPS being build
+         *
+         * @param employee
+         * @return
+         */
+        public Builder addEmployee(Employee employee) {
+            pps.employees.add(employee);
+            return this;
+        }
+
+        /**
+         * Add another project to the PPS
+         * register the specified manager as the manager of the new
+         *
+         * @param project
+         * @param manager
+         * @return
+         */
+        public Builder addProject(Project project, Employee manager) {
+            manager.getAssignedProjects().add(project);
+            if (!pps.employees.contains(manager)) addEmployee(manager);
+            pps.projects.add(project);
+            return this;
+        }
+
+        /**
+         * Add a commitment to work hoursPerDay on the project that is identified by projectCode
+         * for the employee who is identified by employeeNr
+         * This commitment is added to any other commitment that the same employee already
+         * has got registered on the same project,
+         *
+         * @param projectCode
+         * @param employeeNr
+         * @param hoursPerDay
+         * @return
+         */
+        public Builder addCommitment(String projectCode, int employeeNr, int hoursPerDay) {
+            // TODO
+            return this;
+        }
+
+        /**
+         * Complete the PPS being build
+         *
+         * @return
+         */
+        public PPS build() {
+            return this.pps;
+        }
     }
 }
