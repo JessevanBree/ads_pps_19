a2de99a5ab2740b41b2f0cf381e7dd74ec80a808
Romano <rg_asciutto@hotmail.com>
2019-12-01 22:46:02 +0100

    print all planning statistics

--- src/main/java/PPS.java
+++ src/main/java/PPS.java
@@ -79,11 +79,12 @@ public class PPS {
         System.out.printf("%d employees have been assigned to %d projects:\n\n",
                 this.employees.size(), this.projects.size());
 
-        // TODO calculate and display statistics
-        System.out.println(calculateMostInvolvedEmployees().toString());
-        System.out.println(calculateManagedBudgetOverview((e) -> e.getHourlyWage() <= 30).toString());
-        System.out.println(calculateAverageHourlyWage());
-        System.out.println(calculateLongestProject());
+        System.out.printf("1. The average hourly wage of all employees is %f\n", calculateAverageHourlyWage());
+        System.out.printf("2. The longest project is '%s' with %d available working days\n", calculateLongestProject(), calculateLongestProject().getNumWorkingDays());
+        System.out.printf("3. The following employees have the broadest assignment in no less than %d different projects:\n%s\n", 10, calculateMostInvolvedEmployees().toString());
+        System.out.printf("4. The total budget of committed project manpower is %d\n", calculateTotalManpowerBudget());
+        System.out.printf("5. Below is an overview of total managed budget by junior employees (hourly wage <= 30):\n%s\n", calculateManagedBudgetOverview(e -> e.getHourlyWage() <= 30).toString());
+        System.out.printf("6. Below is an overview of cumulative monthly project spends:\n%s", calculateCumulativeMonthlySpends().toString());
     }
 
     /**
